7d14ce7cc79605bf495c45462e5d937b223bd7fd
unknown <ben10a2992@gmail.com>
2020-01-07 20:35:02 +0200

    updating our app with a shop page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "../src/pages/homepage/homePage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from "../src/pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./collection-item.style.scss";
+
+const CollectionItem = ({ id, imageUrl, price, name }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    ></div>
+    <div className="name">{name}</div>
+    <div className="price">{price}</div>
+  </div>
+);
+
+export default CollectionItem;
